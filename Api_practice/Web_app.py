3ca952fda8e497a744a28e0f1db9690950f0fe45
Matias Bregoli <matbregolinh@gmail.com>
2022-02-17 15:04:02 -0300

    Fixed file organisation

--- Api_practice/Web_app.py
+++ Api_practice/Web_app.py
@@ -1,4 +1,4 @@
-from flask	import Flask, render_template, url_for
+from flask import Flask, render_template, url_for
 from Weather import weatherAPI, weather_type_img
 from Forms import addressForm
 
